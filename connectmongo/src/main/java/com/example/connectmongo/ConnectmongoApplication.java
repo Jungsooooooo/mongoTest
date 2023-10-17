8f025d3896ec7434644928f08e6a29040744ce76
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-10-17 15:15:48 +0900

    spring boot & mongoDB 환경 설정

---
+++ connectmongo/src/main/java/com/example/connectmongo/ConnectmongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.connectmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectmongoApplication.class, args);
+	}
+
+}
---
+++ connectmongo/src/test/java/com/example/connectmongo/ConnectmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connectmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
